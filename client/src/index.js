2509374347f6b1af2733d60c593699211850d8b1
James <jimibue@gmail.com>
2022-01-20 09:41:23 -0800

    semantic

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./providers/AuthProvider";
 import { initMiddleware } from "devise-axios";
+// import 'semantic-ui-css/semantic.min.css' BROKE!!!
 
 initMiddleware();
 
